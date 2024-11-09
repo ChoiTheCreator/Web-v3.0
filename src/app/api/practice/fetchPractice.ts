0a66150cf0e3a48f30c41d687973c2eb4f18b351
sangmaaaaan <matthew10164@gmail.com>
2024-11-09 19:45:15 +0900

    feat: add fetchPractice API

--- src/app/api/practice/fetchPractice.ts
+++ src/app/api/practice/fetchPractice.ts
@@ -1,27 +1,27 @@
-// app/api/practice/fetchPractice.ts
+  // app/api/practice/fetchPractice.ts
 
-import axios from "axios";
-import { baseURL } from "..";
+  import axios from "axios";
+  import { baseURL } from "..";
 
-interface FetchPracticeResponse {
-  reqList: {
-    practiceNumber: number;
-    content: string;
-    result: string;
-    solution: string;
-    practiceType: "OX" | "SHORT";
-  }[];
-  summary: string;
-}
-
-// 생성된 문제 목록을 가져오는 API
-export const fetchPractice = async (noteId: number): Promise<FetchPracticeResponse> => {
-  try {
-    const response = await axios.get(`${baseURL}/api/v1/professor/practice/${noteId}`);
-    console.log("response:", response);
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching practice questions:", error);
-    throw error;
+  interface FetchPracticeResponse {
+    reqList: {
+      practiceNumber: number;
+      content: string;
+      result: string;
+      solution: string;
+      practiceType: "OX" | "SHORT";
+    }[];
+    summary: string;
   }
-};
+
+  // 생성된 문제 목록을 가져오는 API
+  export const fetchPractice = async (noteId: number): Promise<FetchPracticeResponse> => {
+    try {
+      const response = await axios.get(`${baseURL}/api/v1/professor/practice/${noteId}`);
+      console.log("response:", response);
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching practice questions:", error);
+      throw error;
+    }
+  };
