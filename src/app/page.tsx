de5c2812ca4a3253a82ef2d34d3d8a455d2c2e3f
sangmaaaaan <matthew10164@gmail.com>
2024-11-13 18:56:34 +0900

    fix: fix route path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,17 @@ const Home = () => {
   const router = useRouter();
 
   const handleProfessorClick = () => {
-    router.push('/home');
+    router.push('/login');
   };
   
   return (
     <div className="flex flex-row bg-bgDeepGray">
       <div className="w-full h-lvh flex flex-col justify-center items-center">
-        <div className="px-8 py-8 font-Pretendard font-semibold leading-[70px] text-[57px] text-custom-green">
+        <div className="px-8 py-8 font-Pretendard font-semibold leading-[70px] text-[57px] text-customGreen">
           회원님의 신분을 선택해주세요
         </div>
         <div className="flex justify-center items-center space-x-4">
-          <button className="bg-primaryGray text-white font-Pretendard text-[18px] hover:bg-custom-green active:bg-custom-green rounded-lg px-16 py-8 space-y-4" onClick={handleProfessorClick}>
+          <button className="bg-primaryGray text-white font-Pretendard text-[18px] hover:bg-customGreen active:bg-custom-green rounded-lg px-16 py-8 space-y-4" onClick={handleProfessorClick}>
             <Image src="professor.svg" alt="logo" width={100} height={100} />
             <p className="font-Pretendard font-normal text-2xl">교수자</p>
           </button>
