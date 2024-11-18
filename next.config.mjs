cdc76e0c867da66d78d5d4743fb147bf2f08162a
sangmaaaaan <matthew10164@gmail.com>
2024-11-19 03:12:59 +0900

    feat: add _redirect to connect with nginx server

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
+import fs from "fs";
+import path from "path";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      const redirectsSourcePath = path.resolve("public", "_redirects");
+      const redirectsDestinationPath = path.resolve(".next", "_redirects");
+
+      // 빌드 후 _redirects 파일 복사
+      config.plugins.push({
+        apply: (compiler) => {
+          compiler.hooks.done.tap("CopyRedirectsPlugin", () => {
+            fs.copyFileSync(redirectsSourcePath, redirectsDestinationPath);
+          });
+        },
+      });
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
