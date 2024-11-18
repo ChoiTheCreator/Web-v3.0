b60451f26b734b061fae2f26bf5703953a3bfd98
sangmaaaaan <matthew10164@gmail.com>
2024-11-18 19:42:47 +0900

    Refactor: fix apiURL

--- src/app/api/folders/index.ts
+++ src/app/api/folders/index.ts
@@ -5,7 +5,7 @@ import axios from "axios";
 
 // 폴더 목록 가져오기
 export const getFolders = async (): Promise<FolderListData[]> => {
-  const response = await axios.get(`${baseURL}/api/v1/folder/`, {
+  const response = await axios.get("https://ai-tutor.co.kr/api/v1/folder/", {
     headers: {
       'Content-Type': 'application/json',
     },
