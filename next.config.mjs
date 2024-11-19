c38098ee74cea89ca3c39399abc14654a3c07824
sangmaaaaan <matthew10164@gmail.com>
2024-11-19 15:08:25 +0900

    chore: add dependency for netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+export default {
+    // 기존 설정 유지...
+    trailingSlash: true,
+  
+    async rewrites() {
+      return [
+        // API 경로는 Netlify에서 NGINX로 전달
+        {
+          source: "/api/v1/:path*",
+          destination: "http://43.201.165.4:8080/:path*",
+        },
+      ];
+    },
+  };
+  
