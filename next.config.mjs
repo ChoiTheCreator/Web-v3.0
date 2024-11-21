6d5726b2783f54e0c14c1d5f3c10de952656d49a
sangmaaaaan <matthew10164@gmail.com>
2024-11-21 15:12:44 +0900

    fix: reset config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,10 @@
-export default {
-    // 기존 설정 유지...
-    trailingSlash: true,
-  
-    async rewrites() {
-      return [
-        // API 경로는 Netlify에서 NGINX로 전달
-        {
-          source: "/api/v1/:path*",
-          destination: "http://43.201.165.4:8080/:path*",
-        },
-      ];
-    },
-  };
-  
+// @ts-check
+ 
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
+}
+ 
+export default nextConfig
