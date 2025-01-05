138fb288d708468509c8097bd391b64bcf2bb26e
ujinsim <syj11243@naver.com>
2025-01-05 15:34:17 +0900

    chore: Update colors from Tailwind config

--- src/app/(routes)/login/page.tsx
+++ src/app/(routes)/login/page.tsx
@@ -8,7 +8,10 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
-const GoogleLoginCTA = dynamic(() => import("@/app/components/utils/GoogleLoginCTA"), { ssr: false });
+const GoogleLoginCTA = dynamic(
+  () => import("@/app/components/utils/GoogleLoginCTA"),
+  { ssr: false }
+);
 
 const Login = () => {
   const [isClient, setIsClient] = useState(false);
@@ -31,8 +34,8 @@ const Login = () => {
   }
 
   return (
-    <div className="w-full h-screen bg-bgGray flex flex-row">
-      <div className="text-mainWhite w-full">
+    <div className="w-full h-screen bg-black-90 flex flex-row">
+      <div className="text-white w-full">
         <div className="fixed py-8 px-8">
           <Link href="/home">
             <Image src="/icon.svg" alt="logo" width={100} height={100} />
@@ -42,11 +45,14 @@ const Login = () => {
           <div className="flex flex-col relative justify-start items-start">
             {isClient && (
               <div className="h-[70px] w-[70px] z-10 ml-[-20px]">
-                <Lottie loop={true} animationData={require("../../../../public/BouncingBall.json")} />
+                <Lottie
+                  loop={true}
+                  animationData={require("../../../../public/BouncingBall.json")}
+                />
               </div>
             )}
             <section className="overflow-hidden">
-              <div className="animate-[textAnimation_8s_infinite]">
+              <div className="animate-[textAnimation_8s_infinite] whitespace-nowrap">
                 <p className="font-Pretendard font-normal text-[28px] block">
                   녹화한 강의 영상을 업로드 해보세요!
                 </p>
@@ -57,7 +63,8 @@ const Login = () => {
                   2단계 : 생성될 문제를 확인하고 수정해보세요
                 </p>
                 <p className="font-Pretendard font-normal text-[28px] block">
-                  3단계 : 마지막으로 생성된 문제와 요약문을 학습자에게 전달해보세요!
+                  3단계 : 마지막으로 생성된 문제와 요약문을 학습자에게
+                  전달해보세요!
                 </p>
                 <p className="font-Pretendard font-normal text-[28px] block">
                   이제 AI Tutor와 함께 질문 및 요약문을 생성하러 가볼까요?
@@ -68,7 +75,7 @@ const Login = () => {
         </div>
       </div>
       <div className="flex flex-col justify-center items-center px-[112px] bg-bgDeepGray">
-        <div className="text-mainWhite font-Pretendard font-normal text-[24px] px-4 py-4">
+        <div className="text-white font-Pretendard font-normal text-[24px] px-4 py-4">
           <p>자기주도학습 시작하기</p>
         </div>
         <GoogleLoginCTA />
