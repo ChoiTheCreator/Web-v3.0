2a81d11a32e1ab7f60380da49182f421f9133fc9
sangmaaaaan <matthew10164@gmail.com>
2025-02-22 20:20:28 +0900

    fix: 아이콘-텍스트 배치 변경 가능한 iconPosition 속성 추가

--- src/app/components/atoms/Button.tsx
+++ src/app/components/atoms/Button.tsx
@@ -12,6 +12,7 @@ interface ButtonProps {
   onMouseLeave?: () => void; // 마우스 리브 핸들러 (옵션)
   type?: "button" | "submit" | "reset"; // 버튼 타입
   variant?: "select" | "next" | "create" | "cancel" | "save"; // 버튼 스타일 종류
+  iconPosition?: "left" | "right"; // 아이콘 위치 설정 (기본값: right)
 }
 
 // 통합된 Button 컴포넌트
@@ -25,12 +26,13 @@ const Button: React.FC<ButtonProps> = ({
   onMouseLeave,
   type = "button",
   variant = "create",
+  iconPosition = "right", // 기본적으로 아이콘을 오른쪽에 배치
 }) => {
   // variant에 따른 스타일 적용
   const getButtonStyles = () => {
     switch (variant) {
       case "select":
-        return `rounded-full ${isSelected ? "bg-mainGreen" : "bg-[#3F3F3F]"}`;
+        return `rounded-full ${isSelected ? "bg-primary" : "bg-[#3F3F3F]"}`;
       case "next":
       case "create":
         return `bg-[#3F3F3F] rounded-full border border-[#565656] hover:bg-[#222222]`;
@@ -52,18 +54,18 @@ const Button: React.FC<ButtonProps> = ({
       onMouseLeave={onMouseLeave}
       disabled={disabled}
     >
+      {/* 아이콘이 앞에 오는 경우 */}
+      {imgSrc && iconPosition === "left" && (
+        <Image src={`/${imgSrc}.svg`} alt="icon" width={16} height={16} className="mr-2" />
+      )}
+
       <p className="font-Pretendard rounded-full font-normal text-center text-base">
         {label}
       </p>
-      {/* 이미지 소스가 존재하면 렌더링 */}
-      {imgSrc && (
-        <Image
-          src={`/${imgSrc}.svg`}
-          alt="icon"
-          width={16}
-          height={16}
-          className="ml-2"
-        />
+
+      {/* 아이콘이 뒤에 오는 경우 */}
+      {imgSrc && iconPosition === "right" && (
+        <Image src={`/${imgSrc}.svg`} alt="icon" width={16} height={16} className="ml-2" />
       )}
     </button>
   );
