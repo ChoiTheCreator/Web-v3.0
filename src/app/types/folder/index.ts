2cee1a0415aa624640be59956617c4561fef6da3
ujinsim <syj11243@naver.com>
2025-03-06 01:15:07 +0900

    fix : folder type 수정

--- src/app/types/folder/index.ts
+++ src/app/types/folder/index.ts
@@ -1,21 +1,22 @@
 export interface FolderListData {
-    folderId: number;
-    folderName: string;
-    professor: string;
-  }
-  
-  export interface FolderListResponse {
-    information: FolderListData[];
-  }
-  
-  export interface PostFolderProps {
-    folderName: string;
-    professorName: string;
-  }
-  
-  export interface PatchFolderProps {
-    folderName: string;
-    professorName: string;
-    folderId: number;
-  }
-  
+  folderId: number;
+  folderName: string;
+  professor: string;
+}
+
+export interface FolderListResponse {
+  information: FolderListData[];
+}
+
+export interface PostFolderProps {
+  token: string;
+  folderName: string;
+  professorName: string;
+}
+
+export interface PatchFolderProps {
+  token: string;
+  folderName: string;
+  professorName: string;
+  folderId: number;
+}
