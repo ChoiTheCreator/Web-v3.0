a61a00983367551ecc73528ee3b2e4e51ea2923b
ujinsim <syj11243@naver.com>
2025-03-06 01:16:24 +0900

    fix : folder api 헤더 토큰값 추가

--- src/app/api/folders/index.ts
+++ src/app/api/folders/index.ts
@@ -1,66 +1,85 @@
-// folderApi.ts
-import { FolderListData, FolderListResponse, PatchFolderProps, PostFolderProps } from "@/app/types/folder";
+import {
+  FolderListData,
+  FolderListResponse,
+  PatchFolderProps,
+  PostFolderProps,
+} from "@/app/types/folder";
 import { baseURL } from "..";
 import axios from "axios";
 import apiClient from "@/app/utils/api";
 
-// 폴더 목록 가져오기
-export const getFolders = async (): Promise<FolderListData[]> => {
+export const getFolders = async (token: string): Promise<FolderListData[]> => {
   const response = await apiClient.get(`${baseURL}/api/v1/folder`, {
     headers: {
-      'Content-Type': 'application/json',
+      Authorization: "Bearer " + token,
     },
   });
 
-  return response.data.information; // `information` 배열만 반환
+  return response.data.information;
 };
 
-// 폴더 생성
-export const createFolder = async ({ folderName, professorName }: PostFolderProps) => {
-const response = await apiClient.post(`${baseURL}/api/v1/folder`, {
+export const createFolder = async ({
+  token,
   folderName,
   professorName,
-}, {
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+}: PostFolderProps) => {
+  const response = await apiClient.post(
+    `${baseURL}/api/v1/folder`,
+    {
+      folderName,
+      professorName,
+    },
+    {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    }
+  );
 
-return response.data;
+  return response.data;
 };
 
-// 폴더 수정
-export const updateFolder = async ({ folderId, folderName, professorName }: PatchFolderProps) => {
-const response = await apiClient.patch(`${baseURL}/api/v1/folder/${folderId}`, {
+export const updateFolder = async ({
+  token,
+  folderId,
   folderName,
   professorName,
-}, {
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+}: PatchFolderProps) => {
+  const response = await apiClient.patch(
+    `${baseURL}/api/v1/folder/${folderId}`,
+    {
+      folderName,
+      professorName,
+    },
+    {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    }
+  );
 
-return response.data;
+  return response.data;
 };
 
-// 폴더 삭제
-export const deleteFolder = async (folderId: number) => {
-const response = await apiClient.delete(`${baseURL}/api/v1/folder/${folderId}`, {
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+export const deleteFolder = async (token: string, folderId: number) => {
+  const response = await apiClient.delete(
+    `${baseURL}/api/v1/folder/${folderId}`,
+    {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    }
+  );
 
-return response.data;
+  return response.data;
 };
 
-// 폴더 이름 가져오기 (test)
-export const fetchFolderName = async () => {
+export const fetchFolderName = async (token: string) => {
   const response = await apiClient.get(`${baseURL}/api/v1/folder/names`, {
     headers: {
-      "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
     },
   });
-  
+
   return response.data;
-  };
+};
