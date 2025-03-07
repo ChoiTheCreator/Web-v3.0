549082ce19d2ca5c3998c7897c921aef796d9990
최원빈 <wonbin109@naver.com>
2025-03-07 22:22:02 +0900

    <Feat> 온보딩 모달, 케러셀 추가 구현 및 필요한 IMG 추가 (loc : Public/IMG)

--- src/app/components/molecules/OnBoardingModal.tsx
+++ src/app/components/molecules/OnBoardingModal.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 
+import Image from 'next/image';
+import OnBoardingCarousel from './OnBoardingCarousel';
 interface OnBoardingModalProps {
   onClose: () => void;
 }
-const OnBoardingModal = () => {
-  return <div></div>;
+
+const OnBoardingModal: React.FC<OnBoardingModalProps> = ({ onClose }) => {
+  return (
+    <div
+      className="fixed flex items-center justify-center bg-black bg-opacity-50"
+      onClick={onClose}
+    >
+      <div className="relative w-[600px] max-w-full bg-transparent">
+        <div className="relative w-full">
+          <Image
+            className="w-full object-cover rounded-t-lg"
+            src="/IMG/onboardingBg.jpeg"
+            alt="온보딩 배경"
+            width={600}
+            height={200}
+          ></Image>
+        </div>
+        <OnBoardingCarousel></OnBoardingCarousel>
+      </div>
+    </div>
+  );
 };
 
 export default OnBoardingModal;
