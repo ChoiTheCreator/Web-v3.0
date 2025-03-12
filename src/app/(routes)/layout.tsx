6727625258620e7294061d5063375a1995f06873
ujinsim <syj11243@naver.com>
2025-03-13 00:48:54 +0900

    feat : 로그인 상태에 따른 리다이렉션 추가

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -7,14 +7,35 @@ import Header from "@/app/components/utils/Header";
 import "@/app/globals.css";
 import { Toaster } from "react-hot-toast";
 import useAuthInterceptor from "../hooks/auth/useAuthInterceptor";
+import { useSession } from "next-auth/react";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { data: session, status } = useSession();
+  const router = useRouter();
   const pathname = usePathname();
   const isLoginPage = pathname.startsWith("/login");
+
+  const [hasShownToast, setHasShownToast] = useState(false);
+
+  useEffect(() => {
+    if (status === "loading") return;
+
+    if (!session?.user?.aiTutorToken && !isLoginPage) {
+      if (!hasShownToast) {
+        toast.error("로그인 상태가 아닙니다");
+        setHasShownToast(true);
+      }
+      router.push("/login");
+    }
+  }, [session, status, hasShownToast, isLoginPage, router]);
+
   useAuthInterceptor();
 
   return (
