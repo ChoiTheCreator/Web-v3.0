2170edbcd81e19660bb1be5eb329c4c3c8200de3
ujinsim <syj11243@naver.com>
2025-03-18 01:52:01 +0900

    feat : 토큰 갱신 실패시 리다이렉트 로직 추가

--- src/app/hooks/auth/useAuthInterceptor.ts
+++ src/app/hooks/auth/useAuthInterceptor.ts
@@ -6,7 +6,7 @@ import { refreshAuthToken } from "@/app/api/auth/[...nextauth]/auth";
 
 const useAuthInterceptor = () => {
   const { data: session, update } = useSession();
-  const BASE_TIMEOUT = 1000 * 60 * 30;
+  const BASE_TIMEOUT = 1000 * 60 * 50;
 
   useEffect(() => {
     if (!session?.user?.aiTutorToken) {
@@ -19,19 +19,30 @@ const useAuthInterceptor = () => {
     const timer = setTimeout(async () => {
       if (!session.user.refreshToken) return;
 
-      const newTokens = await refreshAuthToken(session.user.refreshToken);
+      try {
+        const newTokens = await refreshAuthToken(session.user.refreshToken);
 
-      if (newTokens) {
-        await update({
-          user: {
-            ...session.user,
+        if (newTokens) {
+          await update({
+            user: {
+              ...session.user,
+              aiTutorToken: newTokens.accessToken,
+              refreshToken: newTokens.refreshToken,
+            },
+          });
+
+          await apiClient.post("/api/auth/updateSession", {
             aiTutorToken: newTokens.accessToken,
             refreshToken: newTokens.refreshToken,
-          },
-        });
-        setAuthToken(newTokens.accessToken);
-      } else {
-        toast.error("토큰 갱신 실패, 다시 로그인 해주세요.");
+          });
+
+          setAuthToken(newTokens.accessToken);
+        } else {
+          toast.error("토큰 갱신 실패, 다시 로그인 해주세요.");
+          signOut({ callbackUrl: "/login" });
+        }
+      } catch (error: any) {
+        toast.error(`세션 업데이트 실패: ${error.message}`);
         signOut({ callbackUrl: "/login" });
       }
     }, BASE_TIMEOUT);
