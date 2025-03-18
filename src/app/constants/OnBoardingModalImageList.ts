9a8935440a60cae5d9f3ae6bcbc6579a6e27a2da
WonbinChoi <147716165+ChoiTheCreator@users.noreply.github.com>
2025-03-18 17:02:58 +0900

    <Fix> onBoardingModal.tsx 내부 정적인 slides 배열 constants 폴더에 분리"

---
+++ src/app/constants/OnBoardingModalImageList.ts
@@ -0,0 +1,35 @@
+export const slides = [
+  {
+    background: '/IMG/slide1.png',
+    title: '새 과목 폴더를 만들어보세요',
+    desc: '이미 만든 폴더는 내용을 수정하거나 삭제할 수 있어요!',
+  },
+  {
+    background: '/IMG/slide2.png',
+    title:
+      '새 노트 만들기로 학습 콘텐츠를 생성하고, 노트를 리스트로 관리하세요!',
+    desc: '더 이상 필요없는 노트는 삭제할 수 있어요.',
+  },
+  {
+    background: '/IMG/slide3.png',
+    title:
+      '새 노트 만들기로 학습 콘텐츠를 생성하고, 노트를 리스트로 관리하세요!',
+    desc: '더 이상 필요없는 노트는 삭제할 수 있어요.',
+  },
+  {
+    background: '/IMG/slide4.png',
+    title: '핵심 키워드와 요구사항을 입력해보세요!',
+    desc: '핵심 키워드를 토대로 요약본을 제공해드립니다.',
+  },
+  {
+    background: '/IMG/slide5.png',
+    title: '요약분을 바탕으로 복습 퀴즈와 하이라이트 영상을 제작해보세요!',
+    desc: '효율적인 학습을 위한 자료를 제공해드립니다',
+  },
+
+  {
+    background: '/IMG/slide6.png',
+    title: '필요한 문제 개수와 원하는 문제 유형을 선택하세요!',
+    desc: '만들기 버튼으로 문제를 직접 만들어보세요!',
+  },
+];
