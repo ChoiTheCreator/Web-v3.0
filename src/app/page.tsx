1c85886efa99fb45340b0bd004a8cf40f2a476b7
ujinsim <syj11243@naver.com>
2025-06-23 17:57:35 +0900

    feat : 루트 페이지 진입시 로그아웃

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,24 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
-import React, { useState } from 'react';
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React, { useState, useEffect } from "react";
+import { signOut } from "next-auth/react";
 
 const Home = () => {
   const router = useRouter();
-  const [role, setRole] = useState<'professor' | 'student' | undefined>(
+  const [role, setRole] = useState<"professor" | "student" | undefined>(
     undefined
   );
 
+  useEffect(() => {
+    localStorage.removeItem("aiTutorToken");
+    localStorage.removeItem("refreshToken");
+  }, []);
+
   const handleNextButton = () => {
     if (role !== undefined) {
-      router.push('/login');
+      router.push("/login");
     }
   };
 
@@ -32,14 +38,14 @@ const Home = () => {
             <div className="flex gap-5">
               <div className="flex flex-col gap-2 items-center group">
                 <button
-                  onClick={() => setRole('professor')}
+                  onClick={() => setRole("professor")}
                   className="bg-black-80 text-white font-Pretendard text-xl flex flex-col justify-center items-center gap-2 p-4 rounded-lg transition-all duration-300 ease-in-out "
                 >
                   <Image
                     src={
-                      role === 'professor'
-                        ? 'active_professor.svg'
-                        : 'professor.svg'
+                      role === "professor"
+                        ? "active_professor.svg"
+                        : "professor.svg"
                     }
                     alt="professor-logo"
                     width={124}
@@ -50,17 +56,17 @@ const Home = () => {
 
                 <p
                   className={`h-0.5 w-0 bg-transparent transition-all duration-300 ease-in-out ${
-                    role === 'professor'
-                      ? 'w-full bg-primary'
-                      : 'group-hover:w-full group-hover:bg-primary'
+                    role === "professor"
+                      ? "w-full bg-primary"
+                      : "group-hover:w-full group-hover:bg-primary"
                   }`}
                 ></p>
 
                 <p
                   className={`text-base font-normal transition-all duration-300 ${
-                    role === 'professor'
-                      ? 'text-primary'
-                      : 'group-hover:text-primary text-white'
+                    role === "professor"
+                      ? "text-primary"
+                      : "group-hover:text-primary text-white"
                   }`}
                 >
                   교수자
@@ -69,7 +75,7 @@ const Home = () => {
 
               <div className="flex flex-col gap-2 items-center">
                 <button
-                  onClick={() => setRole('student')}
+                  onClick={() => setRole("student")}
                   className="bg-black-80 text-black-60 font-Pretendard text-xl rounded-lg flex flex-col gap-2 p-4 justify-center items-center"
                   disabled
                 >
@@ -90,8 +96,8 @@ const Home = () => {
               onClick={handleNextButton}
               className={`px-12 py-2 font-semibold rounded-3xl transition-all duration-300 ${
                 role !== undefined
-                  ? 'bg-primary text-white hover:scale-105'
-                  : 'bg-black-80 text-black-70'
+                  ? "bg-primary text-white hover:scale-105"
+                  : "bg-black-80 text-black-70"
               }`}
               disabled={role === undefined}
             >
