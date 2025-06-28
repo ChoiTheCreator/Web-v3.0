35fa71bcae39e36dc10ed0d37fe275425268a264
심유진 <syj11243@naver.com>
2025-06-28 14:58:15 +0900

    feat : folder type 추가

--- src/app/components/utils/Sidebar.tsx
+++ src/app/components/utils/Sidebar.tsx
@@ -15,6 +15,18 @@ const Sidebar: React.FC = () => {
   const [isAuthSet, setIsAuthSet] = useState(false);
   const { open } = useOnboardingstore();
 
+  type Note = {
+    noteId: number;
+    noteName: string;
+  };
+
+  type Folder = {
+    folderId: number;
+    folderName: string;
+    noteCount: number;
+    notesInFolderRes: Note[];
+  };
+
   const pathname = usePathname();
   const params = useParams();
 
@@ -26,11 +38,13 @@ const Sidebar: React.FC = () => {
   const folders = data?.folderNoteDetailList || [];
 
   const [showSections, setShowSections] = useState(false);
-  const [isNoteOpen, setIsNoteOpen] = useState<{ [noteId: number]: boolean }>({});
+  const [isNoteOpen, setIsNoteOpen] = useState<{ [noteId: number]: boolean }>(
+    {}
+  );
 
   const toggleSections = () => setShowSections(!showSections);
   const toggleNote = (noteId: number) => {
-    setIsNoteOpen((prev) => ({
+    setIsNoteOpen(prev => ({
       ...prev,
       [noteId]: !prev[noteId],
     }));
@@ -57,10 +71,17 @@ const Sidebar: React.FC = () => {
         </Link>
 
         <div>
-          <div className={`${isHome ? "bg-black-90" : ""} cursor-pointer transition-colors duration-200`}>
+          <div
+            className={`${
+              isHome ? 'bg-black-90' : ''
+            } cursor-pointer transition-colors duration-200`}
+          >
             <Link href="/home">
               <div className="px-8 py-2 flex flex-row text-center gap-3">
-                <Icon label="ic_side_home" className="w-[20px] h-[20px] my-auto" />
+                <Icon
+                  label="ic_side_home"
+                  className="w-[20px] h-[20px] my-auto"
+                />
                 <p className="text-white">홈</p>
               </div>
             </Link>
@@ -71,7 +92,10 @@ const Sidebar: React.FC = () => {
               className="px-8 py-2 flex flex-row text-center gap-3 cursor-pointer transition-colors duration-200"
               onClick={toggleSections}
             >
-              <Icon label="ic_side_folder" className="w-[20px] h-[20px] m-auto" />
+              <Icon
+                label="ic_side_folder"
+                className="w-[20px] h-[20px] m-auto"
+              />
               <p className="text-white mr-8 flex-shrink-0">강의 과목</p>
               <Icon
                 label="arrow_sidebar"
@@ -82,8 +106,9 @@ const Sidebar: React.FC = () => {
             </div>
 
             {showSections &&
-              folders.map((folder) => {
-                const isSelectedFolder = currentFolderId?.toString() === folder.folderId.toString();
+              folders.map((folder: Folder) => {
+                const isSelectedFolder: boolean =
+                  currentFolderId?.toString() === folder.folderId.toString();
 
                 return (
                   <div key={folder.folderId}>
@@ -92,42 +117,55 @@ const Sidebar: React.FC = () => {
                       className="px-8 py-2 flex justify-between flex-row text-center gap-3 cursor-pointer"
                     >
                       <div className="flex flex-row gap-3">
-                        <Icon label="ic_side_folder" className="w-[20px] h-[20px] my-auto" />
-                        <p className="text-base text-white flex-shrink-0">{folder.folderName}</p>
+                        <Icon
+                          label="ic_side_folder"
+                          className="w-[20px] h-[20px] my-auto"
+                        />
+                        <p className="text-base text-white flex-shrink-0">
+                          {folder.folderName}
+                        </p>
                       </div>
                       {(folder.notesInFolderRes?.length ?? 0) > 0 && (
                         <Icon
                           label="arrow_sidebar"
                           className={`h-[16px] w-[16px] my-auto invert transition-transform duration-300 ${
-                            isNoteOpen[folder.folderId] ? '-rotate-90' : 'rotate-90'
+                            isNoteOpen[folder.folderId]
+                              ? '-rotate-90'
+                              : 'rotate-90'
                           }`}
                         />
                       )}
                     </div>
 
-                    {(folder.notesInFolderRes?.length ?? 0) > 0 && isNoteOpen[folder.folderId] && (
-                      <div className="space-y-1 bg-black-80">
-                        {folder.notesInFolderRes.map((note) => {
-                          const isSelectedNote =
-                            currentFolderId?.toString() === folder.folderId.toString() &&
-                            currentNoteId?.toString() === note.noteId.toString();
+                    {(folder.notesInFolderRes?.length ?? 0) > 0 &&
+                      isNoteOpen[folder.folderId] && (
+                        <div className="space-y-1 bg-black-80">
+                          {folder.notesInFolderRes.map((note: Note) => {
+                            const isSelectedNote: boolean =
+                              currentFolderId?.toString() ===
+                                folder.folderId.toString() &&
+                              currentNoteId?.toString() ===
+                                note.noteId.toString();
 
-                          return (
-                            <Link key={note.noteId} href={`/notes/${folder.folderId}/${note.noteId}/summary`}>
-                              <div
-                                className={`w-full pl-16 text-base py-2 cursor-pointer bg-black-80 ${
-                                  isSelectedNote
-                                    ? 'bg-black-90 text-white'
-                                    : 'text-white hover:bg-black-90'
-                                }`}
+                            return (
+                              <Link
+                                key={note.noteId}
+                                href={`/notes/${folder.folderId}/${note.noteId}/summary`}
                               >
-                                {note.noteName}
-                              </div>
-                            </Link>
-                          );
-                        })}
-                      </div>
-                    )}
+                                <div
+                                  className={`w-full pl-16 text-base py-2 cursor-pointer bg-black-80 ${
+                                    isSelectedNote
+                                      ? 'bg-black-90 text-white'
+                                      : 'text-white hover:bg-black-90'
+                                  }`}
+                                >
+                                  {note.noteName}
+                                </div>
+                              </Link>
+                            );
+                          })}
+                        </div>
+                      )}
                   </div>
                 );
               })}
